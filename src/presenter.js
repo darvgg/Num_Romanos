be836e889b5d3b7b8ae0cb9bea9a190421bf1156
Diego Rios <diegoarv08@hotmail.com>
2025-03-04 22:37:02 -0400

    Cambiando el HTML

--- src/presenter.js
+++ src/presenter.js
@@ -1,15 +1,13 @@
-import sumar from "./sumador";
+import sumar from "./romanos";
 
 const first = document.querySelector("#primer-numero");
-const second = document.querySelector("#segundo-numero");
-const form = document.querySelector("#sumar-form");
+const form = document.querySelector("#romanos-form");
 const div = document.querySelector("#resultado-div");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
   const firstNumber = Number.parseInt(first.value);
-  const secondNumber = Number.parseInt(second.value);
 
-  div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
+  div.innerHTML = "<p>" + "" + "</p>";
 });
