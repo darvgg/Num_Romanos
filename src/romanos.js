25ba1b271a35196fcd6b960d0d1315a7bd6f530d
Diego Rios <diegoarv08@hotmail.com>
2025-03-04 23:16:10 -0400

    Convertir el 2 en romano

--- src/romanos.js
+++ src/romanos.js
@@ -1,7 +1,14 @@
+const valores = [1];
+const simbolos = ["I"];
+
 function convertir (numero){
     let resultado = "";
-    if(numero==1)
-        resultado = "I"
+    for (let i = 0; i < valores.length; i++) {
+        while (numero >= valores[i]) {
+            resultado += simbolos[i];
+            numero -= valores[i];
+        }
+    }
     return resultado;
 }
 
--- src/romanos.test.js
+++ src/romanos.test.js
@@ -4,4 +4,7 @@ describe("Convertir los números", () => {
   it("Debería convertir 1 a I", () => {
     expect(convertir(1)).toEqual("I");
   });
+  it("Debería convertir 2 a I", () => {
+    expect(convertir(2)).toEqual("II");
+  });
 });
